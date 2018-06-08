ba1bcd96091d31643cdba117b3ed61cae1c52218
BigfootN <bigfoot_arch@protonmail.com>
2018-06-08 20:22:55 +0200

    abstractdaofactory

--- src/main/java/com/cours/allo/docteur/factory/AbstractDaoFactory.java
+++ src/main/java/com/cours/allo/docteur/factory/AbstractDaoFactory.java
@@ -16,28 +16,41 @@ import org.apache.commons.logging.LogFactory;
  */
 public abstract class AbstractDaoFactory {
 
-    public static String className = AbstractDaoFactory.class.getName();
-    private static final Log log = LogFactory.getLog(AbstractDaoFactory.class);
+	public static String className = AbstractDaoFactory.class.getName();
+	private static final Log log = LogFactory.getLog(AbstractDaoFactory.class);
 
-    public enum FactoryDaoType {
+	public enum FactoryDaoType {
 
-        MANUAL_LIST_DAO_FACTORY, MANUAL_MAP_DAO_FACTORY;
-    }
+		MANUAL_LIST_DAO_FACTORY, MANUAL_MAP_DAO_FACTORY;
+	}
 
-    public abstract IUtilisateurDao getUtilisateurDao();
+	public abstract IUtilisateurDao getUtilisateurDao();
 
-    public abstract IAdresseDao getAdresseDao();
+	public abstract IAdresseDao getAdresseDao();
 
-    /**
-     * Méthode pour récupérer une factory de DAO
-     *
-     * @param type
-     * @return AbstractDaoFactory
-     */
-    public static AbstractDaoFactory getFactory(FactoryDaoType type) {
-        String methodName = "getFactory";
-        AbstractDaoFactory factory = null;
-        log.debug("FactoryType: " + type);
-        return factory;
-    }
+	/**
+	 * Méthode pour récupérer une factory de DAO
+	 *
+	 * @param type
+	 * @return AbstractDaoFactory
+	 */
+	public static AbstractDaoFactory getFactory(FactoryDaoType type) {
+		String methodName = "getFactory";
+		AbstractDaoFactory factory;
+
+		switch (type) {
+			case MANUAL_LIST_DAO_FACTORY:
+				factory = new ManualListDaoFactory();
+				break;
+			case MANUAL_MAP_DAO_FACTORY:
+				factory = new ManualMapDaoFactory();
+				break;
+			default:
+				factory = null;
+		}
+
+		log.debug("FactoryType: " + type);
+		return factory;
+	}
+
 }
