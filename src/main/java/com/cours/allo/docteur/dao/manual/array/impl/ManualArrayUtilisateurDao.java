40fbb2d66be641466d4983440be5b07b075d0a52
BigfootN <bigfoot_arch@protonmail.com>
2018-06-18 11:47:25 +0200

    finalisation array manual

--- src/main/java/com/cours/allo/docteur/dao/manual/array/impl/ManualArrayUtilisateurDao.java
+++ src/main/java/com/cours/allo/docteur/dao/manual/array/impl/ManualArrayUtilisateurDao.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -23,146 +25,186 @@ import org.apache.commons.logging.LogFactory;
  */
 public class ManualArrayUtilisateurDao implements IUtilisateurDao {
 
-    private static final Log log = LogFactory.getLog(ManualArrayUtilisateurDao.class);
-    public static Utilisateur[] arrayUtilisateursOfDataSource = DaoHelper.getUtilisateursArrayDataSource();
+	private static final Log log = LogFactory.getLog(ManualArrayUtilisateurDao.class);
+	public static Utilisateur[] arrayUtilisateursOfDataSource =
+		DaoHelper.getUtilisateursArrayDataSource();
 
-    @Override
-    public List<Utilisateur> findAllUtilisateurs() {
-        log.debug("Entree de la methode");
-        log.debug("Sortie de la methode");
-        return Arrays.asList(arrayUtilisateursOfDataSource);
-    }
+	@Override
+	public List<Utilisateur> findAllUtilisateurs() {
+		log.debug("Entree de la methode");
+		log.debug("Sortie de la methode");
+		return Arrays.asList(arrayUtilisateursOfDataSource);
+	}
 
-    @Override
-    public Utilisateur findUtilisateurById(int idUtilisateur) {
-        List<Utilisateur> tmp;
-        boolean found;
-        Utilisateur ret;
-        Utilisateur curUser;
-        Iterator<Utilisateur> it;
+	@Override
+	public Utilisateur findUtilisateurById(int idUtilisateur) {
+		List<Utilisateur> tmp;
+		boolean found;
+		Utilisateur ret;
+		Utilisateur curUser;
+		Iterator<Utilisateur> it;
 
-        ret = null;
-        tmp = Arrays.asList(arrayUtilisateursOfDataSource);
-        found = false;
-        it = tmp.iterator();
+		ret = null;
+		tmp = Arrays.asList(arrayUtilisateursOfDataSource);
+		found = false;
+		it = tmp.iterator();
 
-        while (it.hasNext() && !found) {
-            curUser = it.next();
+		while (it.hasNext() && !found) {
+			curUser = it.next();
 
-            if (curUser.getIdUtilisateur() == idUtilisateur) {
-                found = true;
-                ret = curUser;
-            }
-        }
+			if (curUser.getIdUtilisateur() == idUtilisateur) {
+				found = true;
+				ret = curUser;
+			}
+		}
 
-        return ret;
-    }
+		return ret;
+	}
 
-    @Override
-    public List<Utilisateur> findUtilisateursByPrenom(String prenom) {
-        List<Utilisateur> tmp;
-        List<Utilisateur> ret;
-        Utilisateur curUser;
-        Iterator<Utilisateur> it;
+	@Override
+	public List<Utilisateur> findUtilisateursByPrenom(String prenom) {
+		List<Utilisateur> tmp;
+		List<Utilisateur> ret;
+		Utilisateur curUser;
+		Iterator<Utilisateur> it;
 
-        ret = new ArrayList<>();
-        tmp = Arrays.asList(arrayUtilisateursOfDataSource);
-        it = tmp.iterator();
+		ret = new ArrayList<>();
+		tmp = Arrays.asList(arrayUtilisateursOfDataSource);
+		it = tmp.iterator();
 
-        while (it.hasNext()) {
-            curUser = it.next();
+		while (it.hasNext()) {
+			curUser = it.next();
 
-            if (curUser.getPrenom().equals(prenom)) {
-                ret.add(curUser);
-            }
-        }
+			if (curUser.getPrenom().equals(prenom)) {
+				ret.add(curUser);
+			}
+		}
 
-        if (ret.size() == 0)
-            ret = null;
+		if (ret.size() == 0)
+			ret = null;
 
-        return ret;
-    }
+		return ret;
+	}
 
-    @Override
-    public List<Utilisateur> findUtilisateursByNom(String nom) {
-        List<Utilisateur> tmp;
-        List<Utilisateur> ret;
-        Utilisateur curUser;
-        Iterator<Utilisateur> it;
+	@Override
+	public List<Utilisateur> findUtilisateursByNom(String nom) {
+		List<Utilisateur> tmp;
+		List<Utilisateur> ret;
+		Utilisateur curUser;
+		Iterator<Utilisateur> it;
 
-        ret = new ArrayList<>();
-        tmp = Arrays.asList(arrayUtilisateursOfDataSource);
-        it = tmp.iterator();
+		ret = new ArrayList<>();
+		tmp = Arrays.asList(arrayUtilisateursOfDataSource);
+		it = tmp.iterator();
 
-        while (it.hasNext()) {
-            curUser = it.next();
+		while (it.hasNext()) {
+			curUser = it.next();
 
-            if (curUser.getNom().equals(nom)) {
-                ret.add(curUser);
-            }
-        }
+			if (curUser.getNom().equals(nom)) {
+				ret.add(curUser);
+			}
+		}
 
-        if (ret.size() == 0)
-            ret = null;
+		if (ret.size() == 0)
+			ret = null;
 
-        return ret;
-    }
+		return ret;
+	}
 
-    @Override
-    public List<Utilisateur> findUtilisateursByCodePostal(String codePostal) {
-        List<Utilisateur> tmp;
-        List<Utilisateur> ret;
-        Utilisateur curUser;
-        Iterator<Utilisateur> itUser;
-        Iterator<Adresse> itAddr;
-        boolean addrFound;
+	@Override
+	public List<Utilisateur> findUtilisateursByCodePostal(String codePostal) {
+		List<Utilisateur> tmp;
+		List<Utilisateur> ret;
+		Utilisateur curUser;
+		Iterator<Utilisateur> itUser;
+		Iterator<Adresse> itAddr;
+		boolean addrFound;
 
-        ret = new ArrayList<>();
-        tmp = Arrays.asList(arrayUtilisateursOfDataSource);
-        itUser = tmp.iterator();
+		ret = new ArrayList<>();
+		tmp = Arrays.asList(arrayUtilisateursOfDataSource);
+		itUser = tmp.iterator();
 
-        while (itUser.hasNext()) {
-            curUser = itUser.next();
-            itAddr = curUser.getAdresses().iterator();
-            addrFound = false;
+		while (itUser.hasNext()) {
+			curUser = itUser.next();
+			itAddr = curUser.getAdresses().iterator();
+			addrFound = false;
 
-            while (itAddr.hasNext() && !addrFound) {
-                if (itAddr.next().getCodePostal().equals(codePostal)) {
-                    addrFound = true;
-                    ret.add(curUser);
-                }
-            }
-        }
+			while (itAddr.hasNext() && !addrFound) {
+				if (itAddr.next().getCodePostal().equals(codePostal)) {
+					addrFound = true;
+					ret.add(curUser);
+				}
+			}
+		}
 
-        if (ret.size() == 0)
-            ret = null;
+		if (ret.size() == 0)
+			ret = null;
 
-        return ret;
-    }
+		return ret;
+	}
 
-    @Override
-    public Utilisateur createUtilisateur(Utilisateur user) {
-        List<Utilisateur> tmp;
-        int size;
-        int lastId;
+	@Override
+	public Utilisateur createUtilisateur(Utilisateur user) {
+		List<Utilisateur> tmp;
+		int size;
+		int lastId;
 
-        tmp = Arrays.asList(arrayUtilisateursOfDataSource);
-        size = tmp.size();
-        lastId = tmp.get(size - 1).getIdUtilisateur();
-        user.setIdUtilisateur(lastId + 1);
-        tmp.add(user);
+		tmp = Arrays.asList(arrayUtilisateursOfDataSource);
+		size = tmp.size();
+		lastId = tmp.get(size - 1).getIdUtilisateur();
+		user.setIdUtilisateur(lastId + 1);
+		tmp.add(user);
 
-        return user;
-    }
+		return user;
+	}
 
-    @Override
-    public Utilisateur updateUtilisateur(Utilisateur user) {
-        return null;
-    }
+	@Override
+	public Utilisateur updateUtilisateur(Utilisateur user) {
+		List<Utilisateur> tmp;
+		boolean found;
+		Utilisateur ret;
+		Utilisateur curUser;
+		ListIterator<Utilisateur> it;
 
-    @Override
-    public boolean deleteUtilisateur(Utilisateur user) {
-        return false;
-    }
+		tmp = Arrays.asList(arrayUtilisateursOfDataSource);
+		ret = null;
+		found = false;
+		it = tmp.listIterator();
+
+		while (it.hasNext() && !found) {
+			curUser = it.next();
+
+			if (curUser.getIdUtilisateur() == user.getIdUtilisateur()) {
+				it.set(user);
+				found = true;
+				ret = user;
+			}
+		}
+
+		return ret;
+	}
+
+	@Override
+	public boolean deleteUtilisateur(Utilisateur user) {
+		List<Utilisateur> tmp;
+		boolean ret;
+		Utilisateur curUser;
+		ListIterator<Utilisateur> it;
+
+		tmp = Arrays.asList(arrayUtilisateursOfDataSource);
+		ret = false;
+		it = tmp.listIterator();
+
+		while (it.hasNext() && !ret) {
+			curUser = it.next();
+
+			if (curUser.getIdUtilisateur() == user.getIdUtilisateur()) {
+				it.remove();
+				ret = true;
+			}
+		}
+
+		return ret;
+	}
 
 }
