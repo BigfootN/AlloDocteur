3ddbde43e8d77c1c5638aa747c234ec53acca6ec
BigfootN <bigfoot_arch@protonmail.com>
2018-06-18 14:57:50 +0200

    meme erreur

--- src/main/java/com/cours/allo/docteur/dao/manual/array/impl/ManualArrayAdresseDao.java
+++ src/main/java/com/cours/allo/docteur/dao/manual/array/impl/ManualArrayAdresseDao.java
@@ -88,7 +88,7 @@ public class ManualArrayAdresseDao implements IAdresseDao {
 
         ret = new ArrayList<>();
         idx = 0;
-        size = 0;
+        size = arrayAdressesOfDataSource.length;
 
         while (idx < size) {
             if (arrayAdressesOfDataSource[idx].getCodePostal().equals(codePostal))
